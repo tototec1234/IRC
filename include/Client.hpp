728783a8857f4f16401130b00e79e0a59bba3458
tyamaoka <tyamaoka@student.42tokyo.jp>
2026-06-01 12:50:18 +0900

    クラスの責務とclientとchannelの双方向の関連についてコメント

--- include/Client.hpp
+++ include/Client.hpp
@@ -3,14 +3,50 @@
 
 #include <string>
 
+/*
+  clientが所属channelを管理しない設計になっているが、
+  serverstateを親としてみたときchannelとclientは子供同士の関係であり
+  双方向に参照できても問題なさそう
+  client削除時に所属channelからclientを削除する処理が必要になるが
+  channel側でのみclientを管理していると全てのchannelを走査して
+  clientを削除する必要があるためchannel数に比例して処理時間が増える
+  そのため、client側からも所属channelを管理すべきではないか
+*/
+
 class Client {
  public:
   Client();
   ~Client();
 
+  int getFd() const;
+  const std::string& getNick() const;
+  const std::string& getUsername() const;
+  const std::string& getRealname() const;
+  const std::string& getHost() const;
+  std::string getFullPrefix() const;
+  void setUsername(const std::string& username);
+  void setRealname(const std::string& realname);
+  void setHost(const std::string& host);
+  /*
+  ステートの管理が独立したbool値によって行われているが
+  unregistered -> passok -> registered という遷移なので
+  enumで管理するのが自然かもしれない
+  現状のAPIが具体的にどの局面でどのように使い分けられるかがわかりづらい
+  */
+  void setPassOk(bool);
+  bool isPassOk() const;
+  bool isRegistered() const;
+  bool canRegister() const;
+  void markRegistered();
+
  private:
   std::string _nickname;
   std::string _username;
+  std::string _realname;
+  std::string _host;
+  int _fd;
+  bool _passOk;
+  bool _registered;
 };
 
 #endif
--- include/ServerState.hpp
+++ include/ServerState.hpp
@@ -14,10 +14,36 @@ class ServerState {
   ~ServerState();
 
   const std::string& getPassword() const;
+  void addclient(int fd);
+  /*
+    client削除によりchannelの参加者が0になった場合はchannelも削除する
+    その場合、clientは自分の参加しているチャンネルを持つ必要がある
+    フロー：
+      client取得
+      clientから参加しているチャンネルのリストを取得
+      参加しているチャンネルのリストをループ
+      チャンネルからclientを削除
+      チャンネルの参加者が0になったらチャンネル削除
+      client削除
+  */
+  void removeclient(int fd);
+  Client* getClientByFd(int fd);
+  Client* getClientByNick(const std::string& nick);
+  bool nickExists(const std::string& nick) const;
+  void updateNick(Client& client, const std::string& newNick);
+  Channel* getChannel(const std::string& name);
+  Channel* getOrCreateChannel(const std::string& name);
+  void removeChannel(const std::string& name);
 
  private:
   std::string _password;
   std::map<std::string, Channel*> _channels;
+  /*
+  clientの管理は委譲する
+  これはfdからの検索とnickからの検索のためにmapを2つ持っているため
+  serverstateから直接管理するのを避けるためである
+  公開APIは窓口として、実管理はClientRegistryに任せる
+  */
   ClientRegistry _client;
 };
 
